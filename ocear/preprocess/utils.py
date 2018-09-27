3a1d8e05e122e469e876fb6f6778bcb4ac5b72b6
Bart de Goede <bdegoede@gmail.com>
2018-09-27 16:50:56 +0200

    Keep CodeClimate happy

--- ocear/preprocess/utils.py
+++ ocear/preprocess/utils.py
@@ -1,4 +1,4 @@
 def clip_borders(image, clip_percentage=0.1):
     height, width = image.shape
     _h, _w = int(clip_percentage * height), int(clip_percentage * width)
-    return image[_h : height - _h, _w : width - _w]
+    return image[_h:height - _h, _w:width - _w]
